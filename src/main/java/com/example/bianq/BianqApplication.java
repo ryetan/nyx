cc2c66588550e552ebc42f2ab3cefa16e409cc32
Rye Tan （谭强） <qiangtan@trip.com>
2020-03-30 16:21:36 +0800

    init

---
+++ src/main/java/com/example/bianq/BianqApplication.java
@@ -0,0 +1,13 @@
+package com.example.bianq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BianqApplication.class, args);
+	}
+
+}
